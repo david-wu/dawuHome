2a7fc06342fb486ff4741b1eef5eab2efe38d925
David Wu <david.wuu@gmail.com>
2020-03-20 03:21:25 -0700

    style stuff, clean up, move to portfolio

--- src/app/portfolio/portfolio.routes.ts
+++ src/app/portfolio/portfolio.routes.ts
@@ -1,7 +1,27 @@
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PortfolioComponent } from '@src/app/portfolio/portfolio.component';
+import { DemoPageWrapperComponent as FuzzDemoPageComponent } from 'fuzz-demo-app';
 
 export const PortfolioRoutes: Routes = [
-  { path: '', component: PortfolioComponent },
+  {
+    path: '',
+    redirectTo: 'fuzz',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: PortfolioComponent,
+    children: [
+      {
+        path: 'overview',
+        component: FuzzDemoPageComponent,
+      },
+      {
+        path: 'fuzz',
+        component: FuzzDemoPageComponent,
+      },
+    ],
+  },
 ];
