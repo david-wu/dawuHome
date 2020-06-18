f1158b666944f1e82efd1320a3865051a4bcb276
David Wu <david.wuu@gmail.com>
2020-06-18 00:43:28 -0700

    data

--- dist/dawu-home/main.js
+++ dist/dawu-home/main.js
@@ -553,7 +553,7 @@ var FirebaseAuthComponent = /** @class */ (function () {
     }
     FirebaseAuthComponent.prototype.ngOnInit = function () {
         var uiConfig = {
-            signInSuccessUrl: '<url-to-redirect-to-on-success>',
+            signInSuccessUrl: '#/auth-success',
             signInOptions: [
                 // Leave the lines as is for the providers you want to offer your users.
                 window.firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -573,9 +573,45 @@ var FirebaseAuthComponent = /** @class */ (function () {
         // The start method will wait until the DOM is loaded.
         var handle = this.ui.start(this.hostEl.nativeElement, uiConfig);
         console.log('handle', handle);
+        window.firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                // User is signed in.
+                var displayName = user.displayName;
+                var email = user.email;
+                var emailVerified = user.emailVerified;
+                var photoURL = user.photoURL;
+                var uid = user.uid;
+                var phoneNumber = user.phoneNumber;
+                var providerData = user.providerData;
+                user.getIdToken().then(function (accessToken) {
+                    var data = JSON.stringify({
+                        displayName: displayName,
+                        email: email,
+                        emailVerified: emailVerified,
+                        phoneNumber: phoneNumber,
+                        photoURL: photoURL,
+                        uid: uid,
+                        accessToken: accessToken,
+                        providerData: providerData
+                    }, null, '  ');
+                    console.log('data', data);
+                });
+            }
+            else {
+                console.log('signed out');
+                // // User is signed out.
+                // document.getElementById('sign-in-status').textContent = 'Signed out';
+                // document.getElementById('sign-in').textContent = 'Sign in';
+                // document.getElementById('account-details').textContent = 'null';
+            }
+        }, function (error) {
+            console.log(error);
+        });
     };
     FirebaseAuthComponent.prototype.ngOnDestroy = function () {
-        this.ui.delete();
+        if (window.firebaseui.auth.AuthUI.getInstance()) {
+            this.ui.delete();
+        }
     };
     FirebaseAuthComponent.ctorParameters = function () { return [
         { type: _angular_core__WEBPACK_IMPORTED_MODULE_1__["ElementRef"] }
