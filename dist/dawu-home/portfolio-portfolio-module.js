f09445b87c1fcb5a27668f8162b1619bf8614882
David Wu <david.wuu@gmail.com>
2020-07-11 23:57:58 -0700

    data

--- dist/dawu-home/portfolio-portfolio-module.js
+++ dist/dawu-home/portfolio-portfolio-module.js
@@ -96941,6 +96941,17 @@ var PortfolioComponent = /** @class */ (function () {
                         MARKDOWN: { label: 'support markdown-editor' },
                         FAVICON: { label: 'make a favicon' },
                         COMMON: { label: 'components demos' },
+                        PHOTOS: {
+                            label: 'photos app',
+                            childrenById: {
+                                TD1: { label: 'upload photo transaction' },
+                                TD2: { label: 'service worker' },
+                                TD3: { label: 'walk/bike/car distance' },
+                                TD4: { label: 'better models/interfaces' },
+                                TD5: { label: 's2' },
+                                TD6: { label: 's4' },
+                            },
+                        },
                     },
                 },
             },
