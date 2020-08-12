fa8fd98d51f440932bbd6e8c0cdea0295d224d17
David Wu <david.wuu@gmail.com>
2020-08-12 02:03:31 -0700

    use thumbs

--- src/app/photo-gallery/services/exif.service.ts
+++ src/app/photo-gallery/services/exif.service.ts
@@ -6,6 +6,11 @@ import { LocationData } from '@photo-gallery/models/index';
 @Injectable()
 export class ExifService {
 
+  /**
+   * getExifData
+   * @param {File} file
+   * @return {Promise<any>}
+   */
   public getExifData(file: File): Promise<any> {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
@@ -18,21 +23,24 @@ export class ExifService {
     });
   }
 
+  /**
+   * getLocationData
+   * @param {any} exif
+   * @return {LocationData}
+   */
   public getLocationData(exif: any): LocationData {
     if (!exif || !exif.GPSLatitude) {
       return;
     }
     const latitudeMag = exif.GPSLatitude[0] + (exif.GPSLatitude[1] / 60) + (exif.GPSLatitude[2] / 3600);
     const longitudeMag = exif.GPSLongitude[0] + (exif.GPSLongitude[1] / 60) + (exif.GPSLongitude[2] / 3600);
-
     const latitude = (exif.GPSLatitudeRef === 'N') ? latitudeMag : -latitudeMag;
     const longitude = (exif.GPSLongitudeRef === 'E') ? longitudeMag : -longitudeMag;
-    const locationData = LocationData.fromLatLong(
+    return LocationData.fromLatLong(
       latitude,
       longitude,
       { locationSource: 'EXIF' },
     );
-    return locationData;
   }
 
 }
